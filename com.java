05cf82c471415da1308a8d510d6c4e1ef22032c1
Raviteja119 <ravitejathripuraribhotla99@gmail.com>
2024-11-26 15:15:43 +0530

    Add files via upload

---
+++ com.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class com 
+{
+    public static void main(String[] args)
+    {
+      
+      System.out.println("NAME"+":"+args[0]);
+      System.out.println("CLASS"+":"+args[1]);
+      System.out.println("SECTION"+":"+args[2]);
+    }
+}
+    
